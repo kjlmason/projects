f6473d94969b3e26c9fd8c5e42d9d501a7f3e836
kjlmason <48289560+kjlmason@users.noreply.github.com>
2020-05-26 18:21:30 -0400

    visual update

--- guess_the_number/guess.php
+++ guess_the_number/guess.php
@@ -1,21 +1,35 @@
  <!-- guess.php -->
 	<?php
+	if(!isset($_SESSION["setNum"]))
+	{
+		$randNum = rand(1, 100);
+		$_SESSION["setNum"] = $randNum;
+	}
 	
 	if ($_SERVER["REQUEST_METHOD"] == "POST") 
 	{
-		$randNum = rand(1, 10);
-        
-		if ($randNum == $_POST["num"])
+        $setNum = $_SESSION["setNum"];
+		
+		if ($setNum == $_POST["num"])
 		{
 			echo "<h1>Correct!</h1>";
+			echo "<p>I'll think of a new number the next time you guess.</p>";
+			$_SESSION["setNum"] = NULL;
 		}
 		else 
 		{
-			echo "<p class=\"text-danger\">No, I was thinking of $randNum.</p>";
+			//echo "<p class=\"text-danger\">No, I was thinking of $setNum.</p>";
 			
-			$numdiff = abs(($_POST["num"]) - $randNum);
-			echo "<p>Your answer was off by $numdiff!</p>";
-			
+			//$numdiff = abs(($_POST["num"]) - $setNum);
+			//echo "<p>Your answer was off by $numdiff!</p>";
+			if($_POST["num"] < $setNum)
+			{
+				echo "<p class=\"text-danger\">Your answer was too low! Guess again.</p>";
+			}
+			else if($_POST["num"] > $setNum)
+			{
+				echo "<p class=\"text-danger\">Your answer was too high! Guess again.</p>";
+			}
 		}       
 	}
 	?>
--- guess_the_number/index.php
+++ guess_the_number/index.php
@@ -1,22 +1,35 @@
+<?php
+	session_start();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <title>Guess a Number</title>
-	
+	<meta name="description" content="Guess the Number!">
+	<meta name="keywords" content="">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
   </head>
-<body class="bg-dark border-light text-light">
+  
+<body class="bg-dark border-light text-light" style="background:transparent url('image/bg1.png') no-repeat fixed left top /cover;">
 
-<div class="container text-center m-5 mx-auto border rounded">
+<div class="container text-center m-5 mx-auto text-dark">
+	<h1 class="py-5">I'm thinking of a number between 1 and 100.</h1>
 
-	<form method="post" action="index.php">
-		<h1 class="p-4">I'm thinking of a number between 1 and 10.</h1>
-		<p>Your guess? <input type="number" name="num" min="1" max="10" autofocus></p>
-		<input type="submit" value="Guess">
-	</form>
+	<div class="form-group">
+		<form method="post" action="index.php">
+			<label for="num">Your guess?</label>
+			<input class="form-control d-inline-block" type="number" id="num" name="num" min="1" max="100" value="1" style="width:100px" autofocus></br>
+			<div class="py-3"><input class="btn btn-dark" type="submit" value="Submit!"></div>
+		</form>
+	</div>
 
-	<div class="text-primary p-4 font-weight-bold">  
-		<?php include "guess.php";?>
+	<div class="text-primary py-4 font-weight-bold">  
+		<?php
+		include "guess.php";
+		?>
 	</div>
 
 </div>
